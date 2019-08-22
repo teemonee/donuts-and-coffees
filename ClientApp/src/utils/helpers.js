6ec91351db1fb4d1a0beab886a909d085b854578
Tiffani <tiffani.grace.elmore@gmail.com>
2019-08-22 15:19:46 -0500

    add utils constants

--- ClientApp/src/utils/helpers.js
+++ ClientApp/src/utils/helpers.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import Constants from './constants'
 
 const helpers = {
     startNewGame(){
-        return axios.get('/api/Game/GetNewGameSession').then(response => {
+        return axios.get(Constants.NEW_GAME_URL).then(response => {
             return response.data[0];
+        }).catch((error) => {
+            throw new Error(Constants.Game_Fetch_Error);
         })
     }
 };
