e9763c8fda07e21610d9712fbb34a6512f28f30c
Tiffani <tiffani.grace.elmore@gmail.com>
2019-08-22 13:21:26 -0500

    home tests checks board is rendered

---
+++ ClientApp/src/__tests__/Home.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Home } from '../views/Home';
+import Board from '../components/Board'
+
+describe('Home Component', () => {
+    it('renders without crashing', () => {
+        shallow(<Home />);
+    });
+    
+    it('renders board component', () => {
+        const wrapper = shallow(<Home />);
+        
+        expect(wrapper.find(Board)).toHaveLength(1);
+    })
+});
+
+
